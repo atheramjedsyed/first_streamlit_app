be8400b3ede37e53d15f4daa6e32ac4e81be75c3
atheramjedsyed <153247137+atheramjedsyed@users.noreply.github.com>
2023-12-11 11:42:30 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,6 +37,6 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 
-import snowflake.connector.python
+import snowflake.connector
 
 

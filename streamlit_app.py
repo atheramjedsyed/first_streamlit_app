9329c6a0f9df27679be40272bba3cd7bb7590bc8
atheramjedsyed <153247137+atheramjedsyed@users.noreply.github.com>
2023-12-11 15:41:30 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -71,3 +71,11 @@ if streamlit.button('Add Fruit to the List'):
     my_cnx.close()
     streamlit.text(back_from_function)
 
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
+
